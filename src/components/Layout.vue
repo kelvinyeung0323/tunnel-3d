4df36dd2c5322f225eab8615b41ede0a5d0c2f17
kelvin <kelvin_yeung@qq.com>
2023-09-03 14:47:26 +0800

    tunnel monitor project init

---
+++ src/components/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-container>
+        <el-header>
+           
+            <el-dropdown>
+    <span class="el-dropdown-link">
+        <el-avatar>kelvin</el-avatar>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>Profile</el-dropdown-item>
+        <el-dropdown-item>Log out</el-dropdown-item>   
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+        </el-header>
+        <el-main>
+            main
+        </el-main>
+    </el-container>
+</template>
+<script>
+export default{
+
+}
+</script>
